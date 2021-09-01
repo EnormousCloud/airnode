f34f91a85ceb4cb4d82dc03da7818b58391c8200
whatifitrains <victorathome@gmail.com>
2021-09-01 16:10:07 +0300

    better names and a bit of docs

--- airnode-abi/src/lib.rs
+++ airnode-abi/src/lib.rs
@@ -3,40 +3,46 @@ use std::fmt;
 use std::str::FromStr;
 
 /// All Airnode ABI parameters, represended as a map. 
-/// In fact, this is just an alias to `std::collections::HashMap<String, ABIParameter>`
-pub type DecodedMap = std::collections::HashMap<String, ABIParameter>;
+/// In fact, this is just an alias to `HashMap<String, Param>`
+pub type DecodedMap = std::collections::HashMap<String, Param>;
 
 /// Atomic parameter in the Airnode ABI
 #[derive(Debug, PartialEq, Clone)]
-pub enum ABIParameter {
+pub enum Param {
+    /// parameter that embeds array of bytes (dynamic size)
     Bytes {
         name: String,
         value: Vec<u8>,
     },
+    /// parameter that embeds UTF-8 string (dynamic size)
     String {
         name: String,
         value: String,
     },
+    /// parameter that embeds EVM address (160 bits, H160)
     Address {
         name: String,
         value: H160,
     },
+    /// parameters that embeds array of 256 bits values
     Bytes32 {
         name: String,
         value: Vec<U256>,
     },
+    /// parameters that embeds signed 256 bits value (there is no type of I256 in Ethereum primitives)
     Int256 {
         name: String,
         value: U256,
         sign: i32, // we need to store the sign separately as we don't have
     },
+    /// parameters that embeds unsigned 256 bits value
     Uint256 {
         name: String,
         value: U256,
     },
 }
 
-impl ABIParameter {
+impl Param {
     /// returns name of the parameter
     pub fn get_name(&self) -> &str {
         match &self {
@@ -56,16 +62,20 @@ impl ABIParameter {
 
 /// Airnode ABI object that can be encoded into the vector of U256 and decoded from it
 #[derive(Debug, PartialEq, Clone)]
-pub struct ABIParameters {
+pub struct ABI {
+    /// Id of the ABI version. It is always "1" so far
     pub version: u8,
-    pub params: Vec<ABIParameter>,
+    /// List of ABI parameters.
+    pub params: Vec<Param>,
 }
 
-impl ABIParameters {
-    pub fn new(params: Vec<ABIParameter>) -> Self {
+impl ABI {
+    /// constructor of Airnode ABI from the list of parameters
+    pub fn new(params: Vec<Param>) -> Self {
         Self { version: 1, params }
     }
 
+    /// constructor of Airnode ABI with no parameters
     pub fn none() -> Self {
         Self {
             version: 1,
@@ -73,15 +83,17 @@ impl ABIParameters {
         }
     }
 
-    pub fn only(params: ABIParameter) -> Self {
+    /// constructor of Airnode ABI
+    pub fn only(params: Param) -> Self {
         Self {
             version: 1,
             params: vec![params],
         }
     }
 
-    pub fn get(&self, key: &str) -> Option<ABIParameter> {
-        let filtered: Vec<&ABIParameter> = self
+    /// get parameter by its name
+    pub fn get(&self, key: &str) -> Option<Param> {
+        let filtered: Vec<&Param> = self
             .params
             .iter()
             .filter(|&x| x.get_name() == key)
@@ -93,6 +105,7 @@ impl ABIParameters {
         }
     }
 
+    /// turn all parameters into map by parameters name (useful for debugging and testing)
     pub fn into_map(&self) -> DecodedMap {
         let mut map = DecodedMap::new();
         for p in &self.params {
@@ -101,18 +114,20 @@ impl ABIParameters {
         map
     }
 
+    /// encodes ABI into vector or 256 bit values
     pub fn encode(self) -> Vec<U256> {
         let mut out = vec![];
         out
     }
 
+    /// decodes ABI from the vector or 256 bit values
     pub fn decode(input: Vec<U256>) -> Result<Self, ()> {
         Err(())
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
-enum ABIParameterType {
+enum ParamType {
     Bytes,
     String,
     Address,
@@ -125,7 +140,7 @@ enum ABIParameterType {
 // Lower case letters refer to statically sized types
 #[derive(Debug, PartialEq, Clone)]
 #[allow(non_camel_case_types)]
-enum ABIParameterTypeShort {
+enum ParamTypeShort {
     B,
     S,
     a,
@@ -134,69 +149,69 @@ enum ABIParameterTypeShort {
     u,
 }
 
-impl ABIParameterTypeShort {
-    pub fn into(&self) -> ABIParameterType {
+impl ParamTypeShort {
+    pub fn into(&self) -> ParamType {
         match &self {
-            ABIParameterTypeShort::B => ABIParameterType::Bytes,
-            ABIParameterTypeShort::S => ABIParameterType::String,
-            ABIParameterTypeShort::a => ABIParameterType::Address,
-            ABIParameterTypeShort::b => ABIParameterType::Bytes32,
-            ABIParameterTypeShort::i => ABIParameterType::Int256,
-            ABIParameterTypeShort::u => ABIParameterType::Uint256,
+            ParamTypeShort::B => ParamType::Bytes,
+            ParamTypeShort::S => ParamType::String,
+            ParamTypeShort::a => ParamType::Address,
+            ParamTypeShort::b => ParamType::Bytes32,
+            ParamTypeShort::i => ParamType::Int256,
+            ParamTypeShort::u => ParamType::Uint256,
         }
     }
 }
 
-impl ABIParameterType {
-    pub fn into(&self) -> ABIParameterTypeShort {
+impl ParamType {
+    pub fn into(&self) -> ParamTypeShort {
         match &self {
-            ABIParameterType::Bytes => ABIParameterTypeShort::B,
-            ABIParameterType::String => ABIParameterTypeShort::S,
-            ABIParameterType::Address => ABIParameterTypeShort::a,
-            ABIParameterType::Bytes32 => ABIParameterTypeShort::b,
-            ABIParameterType::Int256 => ABIParameterTypeShort::i,
-            ABIParameterType::Uint256 => ABIParameterTypeShort::u,
+            ParamType::Bytes => ParamTypeShort::B,
+            ParamType::String => ParamTypeShort::S,
+            ParamType::Address => ParamTypeShort::a,
+            ParamType::Bytes32 => ParamTypeShort::b,
+            ParamType::Int256 => ParamTypeShort::i,
+            ParamType::Uint256 => ParamTypeShort::u,
         }
     }
 }
 
-impl fmt::Display for ABIParameterType {
+impl fmt::Display for ParamType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", format!("{}", self).to_lowercase())
     }
 }
 
-impl fmt::Display for ABIParameterTypeShort {
+impl fmt::Display for ParamTypeShort {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", format!("{}", self).to_lowercase())
     }
 }
 
-impl FromStr for ABIParameterType {
+impl FromStr for ParamType {
     type Err = ();
-    fn from_str(input: &str) -> Result<ABIParameterType, Self::Err> {
+    fn from_str(input: &str) -> Result<ParamType, Self::Err> {
         match input.to_lowercase().as_str() {
-            "bytes" => Ok(ABIParameterType::Bytes),
-            "string" => Ok(ABIParameterType::String),
-            "address" => Ok(ABIParameterType::Address),
-            "bytes32" => Ok(ABIParameterType::Bytes32),
-            "int256" => Ok(ABIParameterType::Int256),
-            "uint256" => Ok(ABIParameterType::Uint256),
+            "bytes" => Ok(ParamType::Bytes),
+            "string" => Ok(ParamType::String),
+            "address" => Ok(ParamType::Address),
+            "bytes32" => Ok(ParamType::Bytes32),
+            "int256" => Ok(ParamType::Int256),
+            "uint256" => Ok(ParamType::Uint256),
             _ => Err(()),
         }
     }
 }
 
-impl FromStr for ABIParameterTypeShort {
+impl FromStr for ParamTypeShort {
     type Err = ();
-    fn from_str(input: &str) -> Result<ABIParameterTypeShort, Self::Err> {
+    fn from_str(input: &str) -> Result<ParamTypeShort, Self::Err> {
         match input.to_lowercase().as_str() {
-            "B" => Ok(ABIParameterTypeShort::B),
-            "S" => Ok(ABIParameterTypeShort::S),
-            "a" => Ok(ABIParameterTypeShort::a),
-            "b" => Ok(ABIParameterTypeShort::b),
-            "i" => Ok(ABIParameterTypeShort::i),
-            "u" => Ok(ABIParameterTypeShort::u),
+            "B" => Ok(ParamTypeShort::B),
+            "S" => Ok(ParamTypeShort::S),
+            "a" => Ok(ParamTypeShort::a),
+            "b" => Ok(ParamTypeShort::b),
+            "i" => Ok(ParamTypeShort::i),
+            "u" => Ok(ParamTypeShort::u),
             _ => Err(()),
         }
     }
@@ -204,7 +219,7 @@ impl FromStr for ABIParameterTypeShort {
 
 #[cfg(test)]
 mod tests {
-    use super::{ABIParameter, ABIParameters};
+    use super::{Param, ABI};
     use ethereum_types::{H160, U256};
     use hex_literal::hex;
     use rand::{thread_rng, Rng};
@@ -231,23 +246,23 @@ mod tests {
 
     #[test]
     fn it_encodes_decodes_bytes() {
-        let param = ABIParameter::Bytes {
+        let param = Param::Bytes {
             name: rand_str(),
             value: rand_vec(16),
         };
-        let value = ABIParameters::only(param);
-        let decoded = ABIParameters::decode(value.clone().encode()).unwrap();
+        let value = ABI::only(param);
+        let decoded = ABI::decode(value.clone().encode()).unwrap();
         assert_eq!(decoded, value);
     }
 
     #[test]
     fn it_encodes_decodes_string() {
-        let param = ABIParameter::String {
+        let param = Param::String {
             name: rand_str(),
             value: rand_str(),
         };
-        let value = ABIParameters::only(param);
-        let decoded = ABIParameters::decode(value.clone().encode()).unwrap();
+        let value = ABI::only(param);
+        let decoded = ABI::decode(value.clone().encode()).unwrap();
         assert_eq!(decoded, value);
     }
 
@@ -259,24 +274,24 @@ mod tests {
             let r = rand_vec(32);
             input.push(U256::from(into32(&r)));
         }
-        let param = ABIParameter::Bytes32 {
+        let param = Param::Bytes32 {
             name: rand_str(),
             value: input,
         };
-        let value = ABIParameters::only(param);
-        let decoded = ABIParameters::decode(value.clone().encode()).unwrap();
+        let value = ABI::only(param);
+        let decoded = ABI::decode(value.clone().encode()).unwrap();
         assert_eq!(decoded, value);
     }
 
     #[test]
     fn it_encodes_decodes_address() {
         let r = rand_vec(32);
-        let param = ABIParameter::Address {
+        let param = Param::Address {
             name: rand_str(),
             value: H160::from(H160::from_slice(&r)),
         };
-        let value = ABIParameters::only(param);
-        let decoded = ABIParameters::decode(value.clone().encode()).unwrap();
+        let value = ABI::only(param);
+        let decoded = ABI::decode(value.clone().encode()).unwrap();
         assert_eq!(decoded, value);
     }
 
@@ -284,12 +299,12 @@ mod tests {
     fn it_encodes_decodes_uint256() {
         let r = rand_vec(32);
         let input = U256::from(into32(&r));
-        let param = ABIParameter::Uint256 {
+        let param = Param::Uint256 {
             name: rand_str(),
             value: input,
         };
-        let value = ABIParameters::only(param);
-        let decoded = ABIParameters::decode(value.clone().encode()).unwrap();
+        let value = ABI::only(param);
+        let decoded = ABI::decode(value.clone().encode()).unwrap();
         assert_eq!(decoded, value);
     }
 
@@ -298,12 +313,12 @@ mod tests {
         let mut r = rand_vec(32);
         r[0] &= 0b0111_1111; // Unset the first bit to get positive
         let input = U256::from(into32(&r));
-        let param = ABIParameter::Uint256 {
+        let param = Param::Uint256 {
             name: rand_str(),
             value: input,
         };
-        let value = ABIParameters::only(param);
-        let decoded = ABIParameters::decode(value.clone().encode()).unwrap();
+        let value = ABI::only(param);
+        let decoded = ABI::decode(value.clone().encode()).unwrap();
         assert_eq!(decoded, value);
     }
 
@@ -312,18 +327,18 @@ mod tests {
         let mut r = rand_vec(32);
         r[0] |= 0b1000_0000; // Set the first bit to get negative
         let input = U256::from(into32(&r));
-        let param = ABIParameter::Uint256 {
+        let param = Param::Uint256 {
             name: rand_str(),
             value: input,
         };
-        let value = ABIParameters::only(param);
-        let decoded = ABIParameters::decode(value.clone().encode()).unwrap();
+        let value = ABI::only(param);
+        let decoded = ABI::decode(value.clone().encode()).unwrap();
         assert_eq!(decoded, value);
     }
 
     #[test]
     fn it_encodes_empty() {
-        let value = ABIParameters::none().encode();
+        let value = ABI::none().encode();
         let expected: U256 =
             hex!("3100000000000000000000000000000000000000000000000000000000000000").into();
         assert_eq!(vec![expected], value);
@@ -333,8 +348,8 @@ mod tests {
     fn it_decodes_empty() {
         let data: Vec<U256> =
             vec![hex!("3100000000000000000000000000000000000000000000000000000000000000").into()];
-        let res = ABIParameters::decode(data).unwrap();
-        assert_eq!(res, ABIParameters::none());
+        let res = ABI::decode(data).unwrap();
+        assert_eq!(res, ABI::none());
     }
 
     #[test]
@@ -344,8 +359,8 @@ mod tests {
             hex!("54657374416464726573734e616d650000000000000000000000000000000000").into(),
             hex!("0000000000000000000000004128922394c63a204dd98ea6fbd887780b78bb7d").into(),
         ];
-        let res = ABIParameters::decode(data).unwrap();
-        let expected = ABIParameters::only(ABIParameter::Address {
+        let res = ABI::decode(data).unwrap();
+        let expected = ABI::only(Param::Address {
             name: "TestAddressName".to_owned(),
             value: hex!("4128922394C63A204Dd98ea6fbd887780b78bb7d").into(),
         });
@@ -359,8 +374,8 @@ mod tests {
             hex!("54657374496e744e616d65000000000000000000000000000000000000000000").into(),
             hex!("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc18").into(),
         ];
-        let res = ABIParameters::decode(data).unwrap();
-        let expected = ABIParameters::only(ABIParameter::Int256 {
+        let res = ABI::decode(data).unwrap();
+        let expected = ABI::only(Param::Int256 {
             name: "TestIntName".to_owned(),
             value: U256::from(1000),
             sign: -1,
@@ -375,8 +390,8 @@ mod tests {
             hex!("5465737455496e744e616d650000000000000000000000000000000000000000").into(),
             hex!("00000000000000000000000000000000000000000000000000000000000007d0").into(),
         ];
-        let res = ABIParameters::decode(data).unwrap();
-        let expected = ABIParameters::only(ABIParameter::Uint256 {
+        let res = ABI::decode(data).unwrap();
+        let expected = ABI::only(Param::Uint256 {
             name: "TestUIntName".to_owned(),
             value: U256::from(2000),
         });
@@ -392,8 +407,8 @@ mod tests {
             hex!("0000000000000000000000000000000000000000000000000000000000000003").into(),
             hex!("123abc0000000000000000000000000000000000000000000000000000000000").into(),
         ];
-        let res = ABIParameters::decode(data).unwrap();
-        let expected = ABIParameters::only(ABIParameter::Bytes {
+        let res = ABI::decode(data).unwrap();
+        let expected = ABI::only(Param::Bytes {
             name: "TestBytesName".to_owned(),
             value: vec![0x12, 0x3a, 0xbc],
         });
@@ -407,8 +422,8 @@ mod tests {
             hex!("54657374427974657333324e616d650000000000000000000000000000000000").into(),
             hex!("536f6d6520627974657333322076616c75650000000000000000000000000000").into(),
         ];
-        let res = ABIParameters::decode(data).unwrap();
-        let expected = ABIParameters::only(ABIParameter::Bytes32 {
+        let res = ABI::decode(data).unwrap();
+        let expected = ABI::only(Param::Bytes32 {
             name: "TestBytes32Name".to_owned(),
             value: vec![
                 hex!("536f6d6520627974657333322076616c75650000000000000000000000000000").into(),
@@ -426,8 +441,8 @@ mod tests {
             hex!("0000000000000000000000000000000000000000000000000000000000000011").into(),
             hex!("536f6d6520737472696e672076616c7565000000000000000000000000000000").into(),
         ];
-        let res = ABIParameters::decode(data).unwrap();
-        let expected = ABIParameters::only(ABIParameter::String {
+        let res = ABI::decode(data).unwrap();
+        let expected = ABI::only(Param::String {
             name: "TestStringName".to_owned(),
             value: "Some string value".to_owned(),
         });
