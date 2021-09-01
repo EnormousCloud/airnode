8219609dbdf272d7f1bcbc703c8124c1769d1585
whatifitrains <victorathome@gmail.com>
2021-09-01 13:28:03 +0300

    decoding plan

--- airnode-abi/src/lib.rs
+++ airnode-abi/src/lib.rs
@@ -44,6 +44,10 @@ impl ABIParameters {
         Self { version: 1, params }
     }
 
+    pub fn none() -> Self {
+        Self { version: 1, params: vec![] }
+    }
+
     pub fn only(params: ABIParameter) -> Self {
         Self {
             version: 1,
@@ -62,7 +66,7 @@ impl ABIParameters {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum ABIParameterType {
+enum ABIParameterType {
     Bytes,
     String,
     Address,
@@ -75,7 +79,7 @@ pub enum ABIParameterType {
 // Lower case letters refer to statically sized types
 #[derive(Debug, PartialEq, Clone)]
 #[allow(non_camel_case_types)]
-pub enum ABIParameterTypeShort {
+enum ABIParameterTypeShort {
     B,
     S,
     a,
@@ -273,9 +277,115 @@ mod tests {
 
     #[test]
     fn it_encodes_empty() {
-        let value = ABIParameters::new(vec![]).encode();
+        let value = ABIParameters::none().encode();
         let expected: U256 =
             hex!("3100000000000000000000000000000000000000000000000000000000000000").into();
         assert_eq!(vec![expected], value);
     }
+
+    #[test]
+    fn it_decodes_empty() {
+        let data: Vec<U256> = vec![
+            hex!("3100000000000000000000000000000000000000000000000000000000000000").into(),
+        ];
+        let res = ABIParameters::decode(data).unwrap();
+        assert_eq!(res, ABIParameters::none());
+    }
+
+    #[test]
+    fn it_decodes_address() {
+        let data: Vec<U256> = vec![
+            hex!("3161000000000000000000000000000000000000000000000000000000000000").into(),
+            hex!("54657374416464726573734e616d650000000000000000000000000000000000").into(),
+            hex!("0000000000000000000000004128922394c63a204dd98ea6fbd887780b78bb7d").into(),
+        ];
+        let res = ABIParameters::decode(data).unwrap();
+        let expected = ABIParameters::only(ABIParameter::Address {
+            name: "TestAddressName".to_owned(),
+            value: hex!("4128922394C63A204Dd98ea6fbd887780b78bb7d").into(),
+        });
+        assert_eq!(res, expected);
+    }
+    
+    #[test]
+    fn it_decodes_int256() {
+        let data: Vec<U256> = vec![
+            hex!("3169000000000000000000000000000000000000000000000000000000000000").into(),
+            hex!("54657374496e744e616d65000000000000000000000000000000000000000000").into(),
+            hex!("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc18").into(),
+        ];
+        let res = ABIParameters::decode(data).unwrap();
+        let expected = ABIParameters::only(ABIParameter::Int256 {
+            name: "TestIntName".to_owned(),
+            value: U256::from(1000),
+            sign: -1,
+        });
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn it_decodes_uint256() {
+        let data: Vec<U256> = vec![
+            hex!("3175000000000000000000000000000000000000000000000000000000000000").into(),
+            hex!("5465737455496e744e616d650000000000000000000000000000000000000000").into(),
+            hex!("00000000000000000000000000000000000000000000000000000000000007d0").into(),
+        ];
+        let res = ABIParameters::decode(data).unwrap();
+        let expected = ABIParameters::only(ABIParameter::Uint256 {
+            name: "TestUIntName".to_owned(),
+            value: U256::from(2000),
+        });
+        assert_eq!(res, expected);
+    }
+
+
+    #[test]
+    fn it_decodes_bytes() {
+        let data: Vec<U256> = vec![
+            hex!("3142000000000000000000000000000000000000000000000000000000000000").into(),
+            hex!("5465737442797465734e616d6500000000000000000000000000000000000000").into(),
+            hex!("0000000000000000000000000000000000000000000000000000000000000060").into(),
+            hex!("0000000000000000000000000000000000000000000000000000000000000003").into(),
+            hex!("123abc0000000000000000000000000000000000000000000000000000000000").into(),
+        ];
+        let res = ABIParameters::decode(data).unwrap();
+        let expected = ABIParameters::only(ABIParameter::Bytes {
+            name: "TestBytesName".to_owned(),
+            value: vec![0x12, 0x3a, 0xbc],
+        });
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn it_decodes_bytes32() {
+        let data: Vec<U256> = vec![
+            hex!("3162000000000000000000000000000000000000000000000000000000000000").into(),
+            hex!("54657374427974657333324e616d650000000000000000000000000000000000").into(),
+            hex!("536f6d6520627974657333322076616c75650000000000000000000000000000").into(),
+        ];
+        let res = ABIParameters::decode(data).unwrap();
+        let expected = ABIParameters::only(ABIParameter::Bytes32 {
+            name: "TestBytes32Name".to_owned(),
+            value: vec![hex!("536f6d6520627974657333322076616c75650000000000000000000000000000").into()],
+        });
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn it_decodes_string() {
+        let data: Vec<U256> = vec![
+            hex!("3153000000000000000000000000000000000000000000000000000000000000").into(),
+            hex!("54657374537472696e674e616d65000000000000000000000000000000000000").into(),
+            hex!("0000000000000000000000000000000000000000000000000000000000000060").into(),
+            hex!("0000000000000000000000000000000000000000000000000000000000000011").into(),
+            hex!("536f6d6520737472696e672076616c7565000000000000000000000000000000").into(),
+        ];
+        let res = ABIParameters::decode(data).unwrap();
+        let expected = ABIParameters::only(ABIParameter::String {
+            name: "TestStringName".to_owned(),
+            value: "Some string value".to_owned(),
+        });
+        assert_eq!(res, expected);
+    }
+
 }
