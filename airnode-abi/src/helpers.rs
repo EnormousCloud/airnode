0da38105dfa437c4d00d3f24ca4c19f23718eb6d
whatifitrains <victorathome@gmail.com>
2021-09-02 13:44:25 +0300

    int chunk

--- airnode-abi/src/helpers.rs
+++ airnode-abi/src/helpers.rs
@@ -1,12 +1,15 @@
-use ethereum_types::U256;
+use ethereum_types::{U256, U512};
+use std::ops::Add;
+use std::str::FromStr;
 use std::convert::TryInto;
 
-// convert into fixed array of 32 or panic
-// Fix of the size must be at compile time! use carefully, this function panics.
+/// converts array of bytes into fixed array of 32 or panic
+/// Fix of the size must be at compile time! use carefully, this function panics.
 pub fn into32(src: &[u8]) -> [u8; 32] {
     src.try_into().expect(format!("slice with incorrect length of {}", src.len()).as_str())
 }
 
+/// converts array of bytes into vector, padded by 32 bytes with 0 at the end
 pub fn rpad32(src: &[u8]) -> Vec<u8> {
     let mut s = vec![];
     for ch in src.iter() {
@@ -18,6 +21,7 @@ pub fn rpad32(src: &[u8]) -> Vec<u8> {
     s
 }
 
+/// converts array of bytes into vector, padded by 32 bytes with 0 at the begginning
 pub fn lpad32(src: &[u8]) -> Vec<u8> {
     let mut s = vec![];
     while (s.len() + src.len()) % 32 != 0 {
@@ -29,8 +33,8 @@ pub fn lpad32(src: &[u8]) -> Vec<u8> {
     s
 }
 
-// convert string less than 31 characters long.
-// string is padded with 0 to the right.
+/// convert string less than 31 characters long.
+/// string is padded with 0 to the right.
 pub fn str_chunk32(src: &str) -> U256 {
     if src.len() > 31 {
         panic!("string length {} is over 31 bytes", src.len());
@@ -39,8 +43,8 @@ pub fn str_chunk32(src: &str) -> U256 {
     U256::from(into32(&out))
 }
 
-// convert string of unlimited length into array of 256 bits
-// every string is padded with 0 to the right.
+/// convert string of unlimited length into array of 256 bits
+/// every string is padded with 0 to the right.
 pub fn str_chunks(src: &str) -> Vec<U256> {
     let mut chunk: Vec<u8> = vec![];
     let mut out = vec![];
@@ -56,8 +60,26 @@ pub fn str_chunks(src: &str) -> Vec<U256> {
         let v = rpad32(&chunk);
         out.push(U256::from(into32(&v)));
     }
-
     out
+}
+
+/// the first bit of `src` is ignored. sign must be either 1 or -1
+pub fn int_chunk(src: U256, sign: i32) -> U256 {
+    if sign != 1 && sign != -1 {
+        panic!("only sign of 1 or -1 is accepted")
+    }
+    if sign == 1 {
+        U256::from(src)
+    } else {
+        let a = U512::from_str("10000000000000000000000000000000000000000000000000000000000000000").unwrap() - U256::from(src);
+        let U512(ref arr) = a;
+        let mut ret = [0; 4];
+		ret[0] = arr[0];
+		ret[1] = arr[1];
+		ret[2] = arr[2];
+        ret[3] = arr[3];
+		U256(ret)
+    }
 }
 
 #[cfg(test)]
@@ -65,6 +87,13 @@ mod tests {
     use super::*;
     use ethereum_types::U256;
     use hex_literal::hex;
+
+    #[test]
+    fn it_can_encode_negative_int256() {
+        let encoded = int_chunk(U256::from_dec_str("10000000000000000000").unwrap(), -1);
+        let expected: U256 = hex!("ffffffffffffffffffffffffffffffffffffffffffffffff7538dcfb76180000").into();
+        assert_eq!(encoded, expected);
+    }
 
     #[test]
     fn it_can_rpad32() {
