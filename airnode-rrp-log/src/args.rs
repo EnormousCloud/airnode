c4a7e03ef486c58c6d889048a6b57cafb0d68453
EnormousCloud <enormous@webcerebrium.com>
2021-09-11 12:21:06 +0300

    airnode log CLI

---
+++ airnode-rrp-log/src/args.rs
@@ -0,0 +1,47 @@
+use structopt::StructOpt;
+use tracing_subscriber::prelude::*;
+
+#[derive(Debug, StructOpt, Clone)]
+#[structopt(
+    name = "airnode-rrp-log",
+    about = "API3 Airnode smart contract request/response log viewer"
+)]
+pub struct Args {
+    /// Ethereum JSON+RPC HTTP address
+    #[structopt(long, default_value = "http://localhost:8545/", env = "RPC_ENDPOINT")]
+    pub rpc_endpoint: String,
+    /// Ethereum JSON+RPC batch size for reading. Light clients will require smaller sizes
+    #[structopt(long, default_value = "1000", env = "RPC_BATCH_SIZE")]
+    pub rpc_batch_size: u64,
+    /// Number of the first block to start watching
+    #[structopt(long, default_value = "1", env = "RPC_MIN_BLOCK")]
+    pub min_block: u64,
+    /// Max block to stop contract events listening
+    #[structopt(long, env = "RPC_MAX_BLOCK")]
+    pub max_block: Option<u64>,
+    /// API3 secondary voting agent address
+    #[structopt(long, env = "ADDR_CONTRACT")]
+    pub address_contract: String,
+}
+
+pub fn parse() -> anyhow::Result<Args> {
+    dotenv::dotenv().ok();
+    let log_level: String = std::env::var("LOG_LEVEL").unwrap_or("info".to_owned());
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .without_time()
+        .with_ansi(false)
+        .with_level(false)
+        .with_target(false);
+    let filter_layer = tracing_subscriber::EnvFilter::try_from_default_env()
+        .or_else(|_| tracing_subscriber::EnvFilter::try_new(&log_level))
+        .unwrap();
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
+
+    let res = Args::from_args();
+    tracing::debug!("{:?}", res);
+    Ok(res)
+}
---
+++ airnode-rrp-log/src/main.rs
@@ -0,0 +1,81 @@
+pub mod args;
+pub mod reader;
+
+use airnode_events::AirnodeEvent;
+use serde::Serialize;
+use std::collections::BTreeMap;
+use std::str::FromStr;
+use tracing::info;
+use web3::types::{H160, H256};
+
+#[derive(Debug, Clone)]
+pub struct State {
+    // a map of unknown topics
+    pub unknown: BTreeMap<H256, H256>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            unknown: BTreeMap::new(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct Out {
+    block: u64,
+    tx: H256,
+    event: AirnodeEvent,
+}
+
+impl reader::EventHandler for State {
+    fn on(&mut self, l: web3::types::Log) -> () {
+        let evt = AirnodeEvent::from_log(&l).unwrap();
+        match evt {
+            AirnodeEvent::Unknown => {
+                // info!("{:?}", serde_json::to_string(&l).unwrap());
+                // info!("{:?} {:?} {:?}", l.block_number.unwrap(), l.transaction_hash.unwrap(), evt);
+                self.unknown
+                    .insert(l.topics[0], l.transaction_hash.unwrap());
+            }
+            AirnodeEvent::Unclassified => {
+                // Unclassified: this is what is ok to skip silently
+                // info!("{:?} {:?} {:?}", l.block_number.unwrap(), l.transaction_hash.unwrap(), evt);
+            }
+            _ => {
+                let out = Out {
+                    block: l.block_number.unwrap().as_u64(),
+                    tx: l.transaction_hash.unwrap(),
+                    event: evt,
+                };
+                info!("{}", serde_json::to_string(&out).unwrap());
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = match args::parse() {
+        Ok(x) => x,
+        Err(e) => return Err(anyhow::Error::msg(format!("args parsing error {}", e))),
+    };
+    let transport = reader::get_transport(&args.rpc_endpoint).await;
+    let web3 = web3::Web3::new(transport);
+    let chain_id = web3.eth().chain_id().await?.as_u64();
+    let addr_contract = H160::from_str(args.address_contract.as_str()).expect("ADDR_CONTRACT");
+
+    let mut state = State::new();
+    let mut scanner = reader::Scanner::new(
+        chain_id,
+        args.min_block,
+        args.max_block,
+        args.rpc_batch_size,
+    );
+    let _ = scanner.scan_address(&web3, addr_contract, &mut state).await;
+    if state.unknown.len() > 0 {
+        return Err(anyhow::Error::msg("unknown events met"));
+    }
+    Ok(())
+}
