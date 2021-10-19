e140d69011ea44475d2fb933a48fb7e29f1796f9
whitewingshorse <messages@webcerebrium.com>
2021-10-19 23:34:14 +0300

    airnode ui client - initial commit

---
+++ airnode-ui-client/src/components/Header.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import '../index.css';
+
+import { Header } from './Header';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  user: {},
+};
+
+// export const LoggedOut = Template.bind({});
+// LoggedOut.args = {};
