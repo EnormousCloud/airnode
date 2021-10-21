dfaa28c565ba2945f6d69e99bd5c27f8000421a9
whatifitrains <victorathome@gmail.com>
2021-10-21 13:01:56 +0300

    airnode config - helpers

--- airnode-ui-server/src/airnode_config.rs
+++ airnode-ui-server/src/airnode_config.rs
@@ -13,3 +13,22 @@ pub struct AirnodeConfig {
     pub batch_size: Option<u64>,
 }
 
+impl AirnodeConfig {
+    pub fn from(src: Vec<u8>) -> anyhow::Result<Self> {
+        let s = match String::from_utf8(src) {
+            Ok(x) => x,
+            Err(e) => return Err(anyhow::Error::new(e)),
+        };
+        Ok(match serde_json::from_str(&s) {
+            Ok(x) => x,
+            Err(e) => return Err(anyhow::Error::msg(e.to_string())),
+        })
+    }
+    pub fn key(&self) -> Vec<u8> {
+        Vec::from(format!("{}.{}", self.contract_address, self.chain_id).as_bytes())
+    }
+    pub fn as_bytes(&self) -> Vec<u8> {
+        Vec::from(serde_json::to_string(&self).unwrap().as_bytes())
+    }
+}
+
