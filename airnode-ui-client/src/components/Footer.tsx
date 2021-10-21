807f8ee07b36597f5efbc796c6d2383917a04c2a
whitewingshorse <messages@webcerebrium.com>
2021-10-21 11:18:38 +0300

    footer component draft

---
+++ airnode-ui-client/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+interface FooterProps {
+}
+
+export const Footer = (props: FooterProps) => (
+    <footer>
+        <div className="inner">
+            <span className="tool">
+                Airnodes UI Management
+            </span>
+            <span className="copyright">
+                <a href="https://api3.org" target="_blank">API3</a> &copy; 2022
+            </span>
+            <span className="source">
+                <a href="https://github.com/EnormousCloud/airnode">Source on Github</a>
+            </span>
+        </div>
+    </footer>
+);
