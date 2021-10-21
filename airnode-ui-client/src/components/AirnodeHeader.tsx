521d1fa419d1c86d696163fa8bfe54f3683fccd8
whitewingshorse <messages@webcerebrium.com>
2021-10-21 09:48:45 +0300

    airnode selection header draft

---
+++ airnode-ui-client/src/components/AirnodeHeader.tsx
@@ -0,0 +1,35 @@
+interface HeaderProps {
+    onClickSelect?: Function
+    filter?: String
+}
+
+export const AirnodeHeader = (props: HeaderProps) => (
+  <header>
+    <div className="inner">
+        <div className="hd-column">
+            <button className="select" onClick={() => { 
+                props.onClickSelect && props.onClickSelect() 
+            }}>
+                Select Airnode
+            </button>
+        </div>
+        <div className="hd-column-wide">
+            <div className="chain-row">
+                <strong title="EVM Chain" className="chain-name">Rinkeby</strong>
+                &nbsp; / &nbsp; 
+                <strong title="RRP Contract" className="chain-rrp-contract">0x4444...0000</strong>
+                &nbsp; / &nbsp; 
+                <strong title="Airnode Address" className="chain-airnode">0xeeee...0000</strong>
+            </div>
+            <div className="chain-row desktop-only">
+                xPub: &nbsp; 
+                <strong>0x2378832929a2392e3929fc9329292323232acccaa2332320123aa</strong>
+            </div>
+            <div className="chain-row">
+                <span className="desktop-only">Filter: &nbsp; </span>
+                <a href="#">All Endpoints, All Templates, All Functions</a>
+            </div>
+        </div>
+    </div>
+  </header>
+);
