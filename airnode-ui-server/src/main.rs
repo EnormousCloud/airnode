e040d59ea5866eb3f48f8786bbf397effef149c8
whatifitrains <victorathome@gmail.com>
2021-10-22 13:11:00 +0300

    deps and basic args module

--- airnode-ui-server/src/main.rs
+++ airnode-ui-server/src/main.rs
@@ -1,7 +1,15 @@
 pub mod airnode_config;
 pub mod airnode_ops;
 pub mod airnode_state;
+pub mod args;
 
 fn main() {
+    let args = match args::parse() {
+        Ok(x) => x,
+        Err(e) => {
+            panic!("Args parsing error: {}", e);
+        }
+    };
+
     println!("done");
 }
