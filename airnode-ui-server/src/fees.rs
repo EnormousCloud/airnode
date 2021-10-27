7c11e58a6973a51e1b50f4e51e6f13a642cb9d6c
whatifitrains <victorathome@gmail.com>
2021-10-27 12:10:56 +0300

    tx fees: totals

--- airnode-ui-server/src/fees.rs
+++ airnode-ui-server/src/fees.rs
@@ -1,8 +1,9 @@
 use crate::nice;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 use web3::types::TransactionReceipt as Receipt;
-use web3::types::{Transaction, U256};
+use web3::types::{Transaction, H256, U256};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct TxFee {
@@ -42,6 +43,50 @@ impl TxFee {
         pieces.push(format!("Gas Price: {} GWei", nice::dec(self.gas_price, 9)));
         if let Some(usd) = self.usd {
             pieces.push(format!("Est ${}", usd));
+        }
+        pieces.join(", ")
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TxFeeTotal {
+    eth: U256,
+    usd: Option<f64>,
+}
+
+impl TxFeeTotal {
+    pub fn new(events: &Vec<(H256, TxFee)>) -> Self {
+        let mut hs: HashSet<H256> = HashSet::new();
+        let mut eth = U256::from(0);
+        let mut usd = None;
+        events.iter().for_each(|tx| {
+            if !hs.contains(&tx.0) {
+                let fees = &tx.1;
+                eth += fees.gas_price
+                    * match fees.gas_used {
+                        Some(gas_used) => gas_used,
+                        None => fees.gas,
+                    };
+                if let Some(usd_fee) = fees.usd {
+                    match usd {
+                        Some(old) => usd = Some(old + usd_fee),
+                        None => usd = Some(usd_fee),
+                    }
+                }
+                hs.insert(tx.0);
+            }
+        });
+        Self { eth, usd }
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut pieces: Vec<String> = vec![];
+        pieces.push(format!(
+            "Spent {} ETH in fees",
+            nice::float(self.eth, 18, 6)
+        ));
+        if let Some(usd) = self.usd {
+            pieces.push(format!("Est ${:.2}", usd));
         }
         pieces.join(", ")
     }
