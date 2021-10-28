ae790ff9330566e2e01b8f99db1fdf1af8ff9970
whatifitrains <victorathome@gmail.com>
2021-10-28 12:59:16 +0300

    storage ops: tests

--- airnode-ui-server/src/storage_ops.rs
+++ airnode-ui-server/src/storage_ops.rs
@@ -18,11 +18,12 @@ impl LogIndex for Storage {
     fn init(data_dir: &str, airnode: H160) -> Self {
         let mut opts = Options::default();
         opts.create_if_missing(true);
+        opts.create_missing_column_families(true);
         opts.set_use_fsync(false);
         opts.set_keep_log_file_num(1);
 
         let mut cf_opts = Options::default();
-        cf_opts.set_max_write_buffer_number(16);
+        cf_opts.create_if_missing(true);
 
         let file_path = format!("{}/{:?}", data_dir, airnode);
         let cf = ColumnFamilyDescriptor::new("log", cf_opts);
@@ -46,5 +47,63 @@ impl LogIndex for Storage {
             iter.next();
         }
         out
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use airnode_events::AirnodeEvent;
+    use fake::{Fake, Faker};
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+    use std::env;
+    use web3::types::{H160, H256};
+
+    /// converts array of bytes into fixed array of 32 or panic
+    pub fn into32(src: &[u8]) -> [u8; 32] {
+        src.try_into()
+            .expect(format!("slice with incorrect length of {}", src.len()).as_str())
+    }
+
+    /// converts array of bytes into fixed array of 16 or panic
+    pub fn into20(src: &[u8]) -> [u8; 20] {
+        src.try_into()
+            .expect(format!("slice with incorrect length of {}", src.len()).as_str())
+    }
+
+    fn random_operation() -> Operation {
+        let seed = into32(&fake::vec![u8; 32]);
+        let ref mut r = StdRng::from_seed(seed);
+        let topic = H256::from(into32(&fake::vec![u8; 32]));
+        let event = AirnodeEvent::Unknown { topic };
+        Operation {
+            timestamp: Faker.fake_with_rng(r),
+            event,
+            height: Faker.fake_with_rng(r),
+            from: None,
+            block: H256::from(into32(&fake::vec![u8; 32])),
+            transaction_hash: H256::from(into32(&fake::vec![u8; 32])),
+            tx_index: (8..20).fake::<u64>(),
+            log_index: (8..20).fake::<u64>(),
+            fees: None,
+        }
+    }
+
+    #[test]
+    pub fn it_appends_and_reads() {
+        let current_dir = env::current_dir().unwrap();
+        let data_dir = format!("{}/_data", current_dir.as_os_str().to_str().unwrap());
+        let address: H160 = H160::from(into20(&fake::vec![u8; 20]));
+        let db = Storage::init(&data_dir, address);
+        let first = random_operation();
+        db.append(&first);
+        let second = random_operation();
+        db.append(&second);
+        let ops = db.list();
+        assert_eq!(ops.len(), 2);
+        println!("{}", serde_json::to_string(&ops[0]).unwrap());
+        println!("{}", serde_json::to_string(&ops[1]).unwrap());
+        rocksdb::DB::destroy(&rocksdb::Options::default(), &data_dir).unwrap();
     }
 }
