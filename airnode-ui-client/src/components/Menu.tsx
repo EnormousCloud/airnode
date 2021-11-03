d46af12cb8d756346d4137ca92499bb65ba7006e
whitewingshorse <messages@webcerebrium.com>
2021-11-03 20:10:33 +0200

    menu mobile look

--- airnode-ui-client/src/components/Menu.tsx
+++ airnode-ui-client/src/components/Menu.tsx
@@ -1,37 +1,37 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import './Menu.css';
+import "./Menu.css";
 
 // Individual Menu item
 export interface MenuItem {
-    // name of the menu
-    name: string
-    // location reference
-    href: string
-    // count of items
-    counter?: number
-    // whether the item is active
-    active?: boolean
+  // name of the menu
+  name: string;
+  // location reference
+  href: string;
+  // count of items
+  counter?: number;
+  // whether the item is active
+  active?: boolean;
 }
 
 interface MenuProps {
-    // list of items
-    items: Array<MenuItem>
+  // list of items
+  items: Array<MenuItem>;
 }
 
 export const Menu = (props: MenuProps) => (
-    <div className="desktop-menu">
-        <ul>
-            {props.items.map((item: MenuItem) => (
-                <li key={item.name}>
-                    <Link to={item.href} className={(item.active) ? 'active': ''}>
-                        {item.name}
-                        {(item.counter && item.counter > 0) ? (
-                            <span className="badge badge-counter">{item.counter}</span>
-                        ) : null}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </div>
+  <div className="menu">
+    <ul>
+      {props.items.map((item: MenuItem) => (
+        <li key={item.name}>
+          <Link to={item.href} className={item.active ? "active" : ""}>
+            {item.name}
+            {item.counter && item.counter > 0 ? (
+              <span className="badge badge-counter">{item.counter}</span>
+            ) : null}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
