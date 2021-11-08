e2f2e14f052f244b3b9a7e0865f0c64dc93dca4d
whitewingshorse <messages@webcerebrium.com>
2021-11-08 22:54:28 +0200

    Api layer to fetch data

---
+++ airnode-ui-client/src/service/Api.ts
@@ -0,0 +1,8 @@
+const root = () => {
+    return import.meta.env.VITE_API_URL + '';
+};
+
+export const API = {
+    fetchState: () => fetch(root() + '/api/states').then(r => r.json()),
+    fetchOps: (chainId: number, address: string) => fetch(root() + '/api/operations/' + chainId + "/" + address).then(r => r.json())
+};
