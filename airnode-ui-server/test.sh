aa5859ef46e6ac8836648d1f98730954ca5c2782
whatifitrains <victorathome@gmail.com>
2021-11-12 10:47:28 +0200

    rsk testnet prealpha

--- airnode-ui-server/test.sh
+++ airnode-ui-server/test.sh
@@ -21,7 +21,7 @@ export LOG_LEVEL=airnode_ui_server=info,info
     exit
 }
 
-[[ "$1" == "xdai" ]] && {
+[[ "$1" == "xdai-prealpha" ]] && {
     shift
     export RPC_ENDPOINT=$(chainstate --endpoints -t xdai | head -n1)
     [[ "$RPC_ENDPOINT" == "" ]] && { echo "missing RPC endpoint for xdai network. "; exit 1; }
@@ -35,6 +35,23 @@ export LOG_LEVEL=airnode_ui_server=info,info
     cargo run --release -- op list \
         --chain-id 100 \
         --contract-address 0x32D228B5d44Fd18FefBfd68BfE5A5F3f75C873AE
+    exit
+}
+
+[[ "$1" == "rsk-testnet-prealpha" ]] && {
+    shift
+    export RPC_ENDPOINT=$(chainstate --endpoints -t rsk,testnet | head -n1)
+    [[ "$RPC_ENDPOINT" == "" ]] && { echo "missing RPC endpoint for RSK-testnet network. "; exit 1; }
+
+    # adding RRP contract to the configuration
+    cargo run --release -- config add \
+        --contract-address 0x1190a5e1f2afe4c8128fd820a7ac85a95a9e6e3e \
+        --min-block 1817900 \
+        --batch-size 100000
+    # display all RRP operations
+    cargo run --release -- op list \
+        --chain-id 100 \
+        --contract-address 0x1190a5e1f2afe4c8128fd820a7ac85a95a9e6e3e
     exit
 }
 
