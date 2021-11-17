cbbc179db5d8ded1599e1cc233ff81d9658ff01a
whitewingshorse <messages@webcerebrium.com>
2021-11-17 17:08:08 +0200

    rrp admin stories

--- airnode-ui-client/src/screens/Rrp.stories.tsx
+++ airnode-ui-client/src/screens/Rrp.stories.tsx
@@ -9,6 +9,7 @@ import { DataIsReady } from "../service/types";
 
 const chainId = 4;
 const contractAddress = "0xf9c39ec11055508bdda0bc2a0234abbbc09a3dec";
+const opKey = chainId+ '-' + contractAddress;
 
 export default {
   title: "Screens/RRP",
@@ -24,7 +25,7 @@ Airnodes.args = {
   chainId, 
   contractAddress,
   menu: { rrp, airnode: null },
-  nodeState: fullState.filter((x: any) => (x.chain_id == chainId && x.contract_address == contractAddress)),
+  nodeState: fullState.find((x: any) => (x.chain_id == chainId && x.contract_address == contractAddress)),
 };
 
 const TemplateOperations: ComponentStory<typeof RrpOperations> = (args) => (
@@ -36,7 +37,7 @@ Operations.args = {
   chainId, 
   contractAddress,
   dataStatus: DataIsReady,
-  operations,
+  operations: { [opKey]: operations },
   menu: {
     airnode: null,
     rrp: {
@@ -53,18 +54,40 @@ const TemplateAdmins: ComponentStory<typeof RrpAdmins> = (args) => (
   <RrpAdmins {...args} />
 );
 
+const menuAdmins = {
+  airnode: null,
+  rrp: {
+    ...rrp,
+    items: rrp.items.map((item) => ({
+      ...item,
+      active: item.name === "Admins",
+    })),
+  },
+};
+
+export const NoAdmins = TemplateAdmins.bind({});
+NoAdmins.args = {
+  chainId, 
+  contractAddress,
+  nodeState: fullState.find((x: any) => (x.chain_id == chainId && x.contract_address == contractAddress)),
+  menu: menuAdmins,
+  dataStatus: DataIsReady,
+};
+
 export const Admins = TemplateAdmins.bind({});
 Admins.args = {
   chainId, 
   contractAddress,
-  menu: {
-    airnode: null,
-    rrp: {
-      ...rrp,
-      items: rrp.items.map((item) => ({
-        ...item,
-        active: item.name === "Admins",
-      })),
-    },
+  nodeState: {
+    ...fullState.find((x: any) => (x.chain_id == chainId && x.contract_address == contractAddress)),
+    admins: {
+      "0x37c28b4d364cdd2de525c054cf275e437ab3ded9": { address: "0x37c28b4d364cdd2de525c054cf275e437ab3ded9", rank: 10 },
+      "0x00c28b4d364cdd2de525c054cf275e437ab3ded9": { address: "0x0028b4d364cdd2de525c054cf275e437ab3ded9", rank: 8 },
+    }
   },
+  menu: menuAdmins,
+  dataStatus: DataIsReady,
+  operations: {
+    [opKey]: operations.slice(0, 3).map((x:any) => ({ ...x, e: { ... x.e, type: 'SetRank' } })),
+  }
 };
