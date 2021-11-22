5cf9bd2b95112aec1d05a604a55b72579b14dbe1
whitewingshorse <messages@webcerebrium.com>
2021-11-22 20:50:07 +0200

    endpoint screen links

--- airnode-ui-client/src/screens/AirnodeEndpoints.tsx
+++ airnode-ui-client/src/screens/AirnodeEndpoints.tsx
@@ -1,4 +1,5 @@
 import { Footer } from '../components/Footer';
+import { Link } from 'react-router-dom';
 import { MenuPanel, MenuPanelProps } from './../components/MenuPanel';
 import { AirnodeHeader, fromParams } from "./../components/AirnodeHeader";
 
@@ -10,9 +11,15 @@ interface AirnodeEndpointsProps {
     airnodeState: any
 }
 
+const shortened = (x: string, ln: number) => {
+    if (!x || !x.startsWith("0x") || x.length <= ln * 2 + 2) return x;
+    return x.substring(0, ln + 2) + "..." + x.substring(x.length - ln);
+};
+
 export const AirnodeEndpoints = (props: AirnodeEndpointsProps) => {
     const { chainId, contractAddress, provider, airnodeState } = props;
-    const { xpubkey } = airnodeState;
+    const { xpubkey, endpoints } = airnodeState;
+    const opsURL = "/" + chainId + "/" + contractAddress + '/nodes/' + provider + '/operations';
     return (
         <div>
             <AirnodeHeader {...fromParams(chainId, contractAddress, provider)} xpubkey={xpubkey} />
@@ -21,7 +28,21 @@ export const AirnodeEndpoints = (props: AirnodeEndpointsProps) => {
                     <MenuPanel {...props.menu} />
                     <div className="content">
                         <h1>Endpoints</h1>
-                        <pre>{JSON.stringify(airnodeState.endpoints, null, 2)}</pre>
+                        {(Object.keys(endpoints).length > 0) ? (
+                            <ol>
+                                {Object.keys(endpoints).map((ep: string) => (
+                                    <li key={ep}>
+                                        <Link style={{ fontFamily: 'monospace' }} to={ opsURL + '/endpoint/' + ep}>
+                                            {shortened(ep, 12)}
+                                        </Link>
+                                        &nbsp;
+                                        <small className="darken">{endpoints[ep]} request{endpoints[ep] > 1 ? 's':''}</small>
+                                    </li>
+                                ))}
+                            </ol>
+                        ): (
+                            <div className='block-empty'>NO ENDPOINTS</div> 
+                        )}
                     </div>
                 </div>
             </main>
