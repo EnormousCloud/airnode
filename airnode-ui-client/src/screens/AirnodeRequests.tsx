6761547ba193d8a1aedda50a31ab6c5a9b165310
whitewingshorse <messages@webcerebrium.com>
2021-11-22 21:14:26 +0200

    requests screen links and badges

--- airnode-ui-client/src/screens/AirnodeRequests.tsx
+++ airnode-ui-client/src/screens/AirnodeRequests.tsx
@@ -1,4 +1,5 @@
 import { Footer } from '../components/Footer';
+import { Link } from 'react-router-dom';
 import { MenuPanel, MenuPanelProps } from './../components/MenuPanel';
 import { AirnodeHeader, fromParams } from "./../components/AirnodeHeader";
 
@@ -10,9 +11,15 @@ interface AirnodeRequestsProps {
     menu: MenuPanelProps
 }
 
+const shortened = (x: string, ln: number) => {
+    if (!x || !x.startsWith("0x") || x.length <= ln * 2 + 2) return x;
+    return x.substring(0, ln + 2) + "..." + x.substring(x.length - ln);
+};
+
 export const AirnodeRequests = (props: AirnodeRequestsProps) => {
     const { chainId, contractAddress, provider, airnodeState } = props;
-    const { xpubkey } = airnodeState;
+    const { xpubkey, requests } = airnodeState;
+    const opsURL = "/" + chainId + "/" + contractAddress + '/nodes/' + provider + '/operations';
     return (
         <div>
             <AirnodeHeader {...fromParams(chainId, contractAddress, provider)} xpubkey={xpubkey} />
@@ -21,7 +28,26 @@ export const AirnodeRequests = (props: AirnodeRequestsProps) => {
                     <MenuPanel {...props.menu} />
                     <div className="content">
                         <h1>Requests</h1>
-                        <pre>{JSON.stringify(airnodeState.requests, null, 2)}</pre>
+                        {(Object.keys(requests).length > 0) ? (
+                            <ol>
+                                {Object.keys(requests).map((rq: string) => (
+                                    <li key={rq}>
+                                        <Link style={{ fontFamily: 'monospace' }} to={ opsURL + '/request/' + rq}>
+                                            {shortened(rq, 16)}
+                                        </Link>
+                                        &nbsp;
+                                        {(requests[rq].fulfill) ? (
+                                            <span className='badge badge-ok'>Fulfilled</span>
+                                        ): null}
+                                        {(requests[rq].fail) ? (
+                                            <span className='badge badge-error'>Fail</span>
+                                        ): null}
+                                    </li>
+                                ))}
+                            </ol>
+                        ): (
+                            <div className='block-empty'>NO ENDPOINTS</div> 
+                        )}
                     </div>
                 </div>
             </main>
