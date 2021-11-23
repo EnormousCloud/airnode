3f1cdfd53835837b07d24bc22645a4da839797c7
whitewingshorse <messages@webcerebrium.com>
2021-11-23 16:17:24 +0200

    withdrawals requests

--- airnode-ui-client/src/screens/AirnodeWithdrawals.tsx
+++ airnode-ui-client/src/screens/AirnodeWithdrawals.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Footer } from '../components/Footer';
 import { MenuPanel, MenuPanelProps } from './../components/MenuPanel';
 import { AirnodeHeader, fromParams } from "./../components/AirnodeHeader";
@@ -10,9 +11,18 @@ interface AirnodeWithdrawalsProps {
     menu: MenuPanelProps
 }
 
+const shortened = (x: string, ln: number) => {
+    if (!x || !x.startsWith("0x") || x.length <= ln * 2 + 2) return x;
+    return x.substring(0, ln + 2) + "..." + x.substring(x.length - ln);
+};
+
 export const AirnodeWithdrawals = (props: AirnodeWithdrawalsProps) => {
     const { chainId, contractAddress, provider, airnodeState } = props;
-    const { xpubkey } = airnodeState;
+    const { xpubkey, requests } = airnodeState;
+    const opsURL = "/" + chainId + "/" + contractAddress + '/nodes/' + provider + '/operations';
+    const filtered = Object.keys(requests).filter((rq: string) => (
+        !!requests[rq].withdraw
+    ));
     return (
         <div>
             <AirnodeHeader {...fromParams(chainId, contractAddress, provider)} xpubkey={xpubkey} />
@@ -21,7 +31,26 @@ export const AirnodeWithdrawals = (props: AirnodeWithdrawalsProps) => {
                     <MenuPanel {...props.menu} />
                     <div className="content">
                         <h1>Withdrawals</h1>
-                        <pre>{JSON.stringify(airnodeState.requests, null, 2)}</pre>
+                        {(filtered.length > 0) ? (
+                            <ol>
+                                {filtered.map((rq: string) => (
+                                    <li key={rq}>
+                                        <Link style={{ fontFamily: 'monospace' }} to={ opsURL + '/request/' + rq}>
+                                            {shortened(rq, 16)}
+                                        </Link>
+                                        &nbsp;
+                                        {(requests[rq].fulfill) ? (
+                                            <span className='badge badge-ok'>Fulfilled</span>
+                                        ): null}
+                                        {(requests[rq].fail) ? (
+                                            <span className='badge badge-error'>Fail</span>
+                                        ): null}
+                                    </li>
+                                ))}
+                            </ol>
+                        ): (
+                            <div className='block-empty'>NO WITHDRAWALS</div> 
+                        )}
                     </div>
                 </div>
             </main>
