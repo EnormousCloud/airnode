0c38b19e1cb5dc69c3212e148946064f98da8549
whitewingshorse <messages@webcerebrium.com>
2021-12-01 16:49:44 +0200

    better balance look

--- airnode-ui-client/src/service/Balance.ts
+++ airnode-ui-client/src/service/Balance.ts
@@ -1,11 +1,21 @@
+import { utils } from 'ethers';
+
 export interface NiceBalance {
     balance: string
     symbol: string
 }
 
+const pad6 = (x: string): string => {
+    // this is far from perfect, we decided to wait for uiValue from the server side
+    const parts = utils.formatEther(x).split(".");
+    if (parts.length != 2) return x;
+    if (parts[1].length > 6) return x;
+    return parts[0] + '.' + parts[1].substring(0, 6);
+}
+
 const niceBalance = (x: string, _decimals: number) => {
     if (x === '0x0') return '0';
-    return x;
+    return pad6(x);
 }
 
 export const parseBalance = (obj: any): NiceBalance => {
