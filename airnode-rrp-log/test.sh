24e5d9b3eeb72f33afe9d21f1c5a1e52aaf43793
EnormousCloud <enormous@webcerebrium.com>
2021-12-15 20:55:33 +0200

    test.sh for v0.3 deployments

--- airnode-rrp-log/test.sh
+++ airnode-rrp-log/test.sh
@@ -1,36 +1,51 @@
-[[ "$ENORMOUS_RPC_API_TOKEN" == "" ]] && {
-    echo "ENORMOUS_RPC_API_TOKEN is not provided"
-    exit 1
-}
-
 [[ "$1" == "xdai" ]] && {
-    export RPC_ENDPOINT=http://xdai.enormous.cloud/$ENORMOUS_RPC_API_TOKEN
-    export RPC_BATCH_SIZE=10000
+    shift
+    export RPC_ENDPOINT=$(chainstate --endpoints -t xdai | head -n1)
+    [[ "RPC_ENDPOINT" == "" ]] && { echo "missing RPC endpoint for rinkeby network. "; exit 1; }
+    export RPC_BATCH_SIZE=100000
     export RPC_MIN_BLOCK=13796900
     export ADDR_CONTRACT=0x32D228B5d44Fd18FefBfd68BfE5A5F3f75C873AE
-    cargo run --release
+    cargo run --release -- $@
+    exit
 }
 
 [[ "$1" == "rinkeby" ]] && {
-    export RPC_ENDPOINT=http://rinkeby.enormous.cloud/$ENORMOUS_RPC_API_TOKEN
+    shift
+    export RPC_ENDPOINT=$(chainstate --endpoints -t rinkeby,infura | head -n1)
+    [[ "RPC_ENDPOINT" == "" ]] && { echo "missing RPC endpoint for rinkeby network. "; exit 1; }
+
     export RPC_BATCH_SIZE=50000
     export RPC_MIN_BLOCK=7812260
     export ADDR_CONTRACT=0xf9c39ec11055508bdda0bc2a0234abbbc09a3dec
-    cargo run --release
+    cargo run --release -- $@
+    exit
 }
 
 [[ "$1" == "rinkeby2" ]] && {
-    # something from Hackathon
-    export RPC_ENDPOINT=http://rinkeby.enormous.cloud/$ENORMOUS_RPC_API_TOKEN
+    shift
+    export RPC_ENDPOINT=$(chainstate --endpoints -t rinkeby,infura | head -n1)
+    [[ "RPC_ENDPOINT" == "" ]] && { echo "missing RPC endpoint for rinkeby network. "; exit 1; }
+
     export RPC_BATCH_SIZE=50000
     export RPC_MIN_BLOCK=8494000
     export ADDR_CONTRACT=0xb8e96200a1290907436d928bcc3c7ff18e7f4ae6
-    cargo run --release
+    cargo run --release -- $@
+    exit
+}
+
+[[ "$1" == "rinkeby0.3" ]] && {
+    shift
+    export RPC_ENDPOINT=$(chainstate --endpoints -t rinkeby,infura | head -n1)
+    [[ "RPC_ENDPOINT" == "" ]] && { echo "missing RPC endpoint for rinkeby network. "; exit 1; }
+
+    export RPC_BATCH_SIZE=1000000
+    export RPC_MIN_BLOCK=9780500
+    export ADDR_CONTRACT=0xC11593B87f258672b8eB02d9A723a429b15E9E03
+    cargo run --release -- $@
+    exit
 }
 
 [[ "$1" == "" ]] && {
-    echo "missing parameter: xdai,rinkeby,rinkeby2"
+    echo "missing parameter: xdai,rinkeby,rinkeby2,rinkeby0.3"
     exit 1
 }
-
-
